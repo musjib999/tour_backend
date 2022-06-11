c96f6795a5c1e3e9a940ce69967ae501e6c80e21
Musa Jibril <musjib999@gmail.com>
2022-06-11 18:33:31 +0100

    reduce image size and upload

--- functions/src/place/functions/createPlace.ts
+++ functions/src/place/functions/createPlace.ts
@@ -1,11 +1,12 @@
 import * as functions from "firebase-functions";
 
 const multer = require("multer-firebase");
-import * as express from "express";
+import express = require("express");
 import { uploadImage } from "../storage";
 import { v4 as uuidv4 } from 'uuid';
-import { reduceImageQuality } from "../../helpers/imageHelper";
+// import { reduceImageQuality } from "../../helpers/imageHelper";
 import { print } from "../../settings";
+import { imageResizer } from "../../helpers/imageResizer";
 //jimp, firebase
 
 const storage = multer.diskStorage({})
@@ -14,27 +15,21 @@ const upload = multer({ storage: storage });
 const app = express();
 
 app.post('/', upload.single('image'), async (req, res, next) => {
-    let images: string[] = [];
+    // let images: string[] = [];
     // const file = req.file;
     if (req.file) {
         try {
             let id: string = uuidv4();
-            const ext = req.file!.originalname.split('.').pop();
-
-            await uploadImage(req.file.path, `places/${req.file.filename}/${req.file.filename}-full.${ext}`, id).then(async (originalImage) => {
-                originalImage == undefined ? images : images.push(originalImage);
-                res.status(200).json({ status: 'success', payload: images, message: 'Image uploaded successful' })
-                await reduceImageQuality(req.file!.path, req.file!.filename, ext == undefined ? 'jpeg' : ext, 75).then(async(value) => {
-                    console.log(value);
-                    if (value == '') {
-                        print(`[uploadPlace] Error trying to upload image`);
-                        res.status(500).json({ status: 'failed', payload: null, message: 'Error trying to upload imag' });
-                    } else {
-                        print('Image reduced successfully');
-                    //    await uploadImage(value, `places/${file!.filename}/${file!.filename}-normal.${ext}`, id);
-                    }
+            const ext = req.file.originalname.split('.').pop();
+            await imageResizer(req.file, id, ext ?? 'jpg').then((paths: string[]) => {
+                paths.forEach(async (path) => {
+                    const filename: string = path.split('/')[path.split('/').length - 1];
+                    const imageUrl = await uploadImage(path, `places/${filename.split('@')[0]}/${filename}`, id);
+                    print(imageUrl!);
                 });
             });
+            res.status(200).json({ status: 'success', payload: req.file, message: 'Image uploaded successful' })
+
         } catch (error) {
             print(`[uploadPlace] Error => ${error} `);
             res.status(500).json({ status: 'failed', payload: null, message: `Failed to uploaded image ==> ${error}` });
